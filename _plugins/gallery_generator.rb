63e2ceae63e6db9a96fbb44685794435288df2c4
vincent <vincentcharlebois@gmail.com>
2024-12-06 12:05:05 -0500

    Exclude 'header.webp' from gallery image selection in GalleryGenerator

--- _plugins/gallery_generator.rb
+++ _plugins/gallery_generator.rb
@@ -1,22 +1,22 @@
 class GalleryGenerator < Jekyll::Generator
-    safe true
-  
-    def generate(site)
-      if site.collections['projects']
-        site.collections['projects'].docs.each do |project|
-          if project.data['gallery_folder']
-            gallery_folder = File.join(site.source, project.data['gallery_folder'])
-            if Dir.exist?(gallery_folder)
-              images = Dir.entries(gallery_folder).select { |f| f.match?(/\.(jpg|jpeg|png|webp)$/i) }
-              project.data['gallery'] = images.map do |image|
-                {
-                  'src' => File.join(site.baseurl, project.data['gallery_folder'], image),
-                  'caption' => File.basename(image, File.extname(image)).gsub('_', ' ').capitalize
-                }
-              end
+  safe true
+
+  def generate(site)
+    if site.collections['projects']
+      site.collections['projects'].docs.each do |project|
+        if project.data['gallery_folder']
+          gallery_folder = File.join(site.source, project.data['gallery_folder'])
+          if Dir.exist?(gallery_folder)
+            images = Dir.entries(gallery_folder).select { |f| f.match?(/\.(jpg|jpeg|png|webp)$/i) && f != 'header.webp' }
+            project.data['gallery'] = images.map do |image|
+              {
+                'src' => File.join(site.baseurl, project.data['gallery_folder'], image),
+                'caption' => File.basename(image, File.extname(image)).gsub('_', ' ').capitalize
+              }
             end
           end
         end
       end
     end
   end
+end
