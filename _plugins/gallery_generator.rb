956565cb30bd85494a8eb0e60c74d181b30af967
vincent <vincentcharlebois@gmail.com>
2025-03-10 00:23:08 -0400

    🪾

--- _plugins/gallery_generator.rb
+++ _plugins/gallery_generator.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class GalleryGenerator < Jekyll::Generator
   safe true
 
@@ -7,12 +9,20 @@ class GalleryGenerator < Jekyll::Generator
         if project.data['gallery_folder']
           gallery_folder = File.join(site.source, project.data['gallery_folder'])
           pdf_folder = File.join(gallery_folder, 'pdf') # Look for a 'pdf' subfolder
+          captions_file = File.join(gallery_folder, 'captions.yml') # Look for captions.yml
+
+          
+          # Load captions if the YAML file exists
+          captions = {}
+          if File.exist?(captions_file)
+            captions = YAML.load_file(captions_file) || {}
+          end
 
           if Dir.exist?(gallery_folder)
             images = Dir.entries(gallery_folder).select { |f| f.match?(/\.(jpg|jpeg|png|webp)$/i) && f != 'header.webp' }
             project.data['gallery'] = images.map do |image|
               file_name = File.basename(image, File.extname(image))
-              is_pdf_preview = image.downcase == 'pdf.webp'  # Ensure it's exactly "pdf.webp"
+              is_pdf_preview = image.downcase == 'pdf.webp'
 
               pdf_src = nil
               if is_pdf_preview
@@ -24,7 +34,7 @@ class GalleryGenerator < Jekyll::Generator
 
               {
                 'src' => File.join(site.baseurl, project.data['gallery_folder'], image),
-                'caption' => file_name.gsub('_', ' ').capitalize,
+                'caption' => captions[file_name] || file_name.gsub('_', ' ').capitalize, 
                 'type' => is_pdf_preview ? 'pdf' : 'image',
                 'pdf_src' => pdf_src  # Store the PDF link
               }
